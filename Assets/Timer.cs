d1625fc3ad0d738221f019e0ef1aeff20fa0a00c
DestinyChescappio <chescappiodestiny@gmail.com>
2025-04-08 23:00:38 -0400

    scripts & box colliders

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
-using TMPro; 
+using TMPro;
+using UnityEngine.SceneManagement;
+
 public class Timer : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI timerText;
-    float elapsedTime;
-    // Update is called once per frame
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private float remainingTime = 20f; // ⏳ Start at 20 seconds
+
+    private bool hasLost = false;
+
     void Update()
     {
-        elapsedTime += Time.deltaTime; 
-        int minutes = Mathf.FloorToInt(elapsedTime / 60);
-        int seconds = Mathf.FloorToInt(elapsedTime % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds); 
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+
+            if (remainingTime <= 0 && !hasLost)
+            {
+                remainingTime = 0;
+                hasLost = true;
+                SceneManager.LoadScene("LosePage");
+            }
+
+            int minutes = Mathf.FloorToInt(remainingTime / 60);
+            int seconds = Mathf.FloorToInt(remainingTime % 60);
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
     }
 }
